204655699b05779eefd7223dbf3c3006ada20f95
Qwerty <oshovsky.vlad@yandex.ru>
2020-12-16 19:15:32 +0800

    Commit 2

--- src/main/java/ru/geekbrains/boot/model/Product.java
+++ src/main/java/ru/geekbrains/boot/model/Product.java
@@ -1,10 +1,15 @@
 package ru.geekbrains.boot.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     private Long id;
     private String title;
-    private int score;
+    private int cost;
 }
+
